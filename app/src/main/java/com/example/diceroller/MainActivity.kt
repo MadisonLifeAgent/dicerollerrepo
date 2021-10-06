fb79d50cee52b54aacfe318cf9250ac86b8042de
Fue Moua <MadisonLifeAgent@gmail.com>
2021-10-05 22:56:54 -0500

    replaced text view with imageview

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,16 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         // button listener
         rollButton.setOnClickListener {
-            // this is where the dice roll where be stored and how it will be displayed
-            //val resultTextView: TextView = findViewById(R.id.textView2)
-
-            // set the value of the resultTextView to be displayed
             rollDice()
-
-            // display Toast (brief message) - A toast is a view containing a quick little message for the user.
-            /*val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()*/
-            // can also be written as - Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -37,14 +28,14 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
 
         // gets the text view where dice roll will be displayed
-        val resultTextView: TextView = findViewById(R.id.textView2)
+        val resultImageView: ImageView = findViewById(R.id.imageView)
 
         // converts dice roll to string for display in textview
-        resultTextView.text = diceRoll.toString()
+        //resultImageView = diceRoll.toString()
     }
 }
 // dice class that gets random roll value
-class Dice(val numSides: Int) {
+class Dice(private val numSides: Int) {
     fun roll(): Int {
         return(1..numSides).random()
     }
